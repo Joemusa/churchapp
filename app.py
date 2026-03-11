3ece883f7c7d63c7e490b80366f4684b72989d7d
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-03-11 08:22:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,26 @@ st.title("Church Member Verification")
 
 digits = st.text_input("Enter the last 4 digits of your cellphone number")
 
+# Create credentials once
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open_by_key(
+    "1k2mBMHROvmht5aaQjPQenHtwWVl9Y_h-gey5EIIAwLQ"
+).worksheet("Sheet1")
+
 if st.button("Submit"):
 
     if len(digits) == 4 and digits.isdigit():
-
-        scope = [
-            "https://www.googleapis.com/auth/spreadsheets",
-            "https://www.googleapis.com/auth/drive"
-        ]
-
-        # Authenticate using Streamlit Secrets
-        creds = Credentials.from_service_account_info(
-            st.secrets["gcp_service_account"],
-            scopes=scope
-        )
-
-        client = gspread.authorize(creds)
-
-        sheet = client.open_by_key(
-            "1k2mBMHROvmht5aaQjPQenHtwWVl9Y_h-gey5EIIAwLQ"
-        ).worksheet("Sheet1")
 
         sheet.append_row([
             datetime.now().strftime("%Y-%m-%d %H:%M"),
